aeb4bfdc1de8abce6168df87e71abfea4077267a
Robbie <robbie331@>
2025-07-15 14:08:42 +0900

    Level1 - 1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,21 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0);
+
+        System.out.println("operator = " + operator);
 
     }
 }
