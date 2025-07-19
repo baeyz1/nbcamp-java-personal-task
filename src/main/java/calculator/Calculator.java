fa46dfc67177ed6a262daace16582ef45fabe23c
Robbie <robbie331@>
2025-07-19 11:30:52 +0900

    Level2 -1 Exception 클래스를 throw해야하는데, 어느 구간에서 어떤 방식으로 사용해야 하는지 잘 모르겠어서 해결하지 못함.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,38 @@
 package calculator;
 
-public class Calculator {
+import java.util.ArrayList;
+import java.util.List;
 
-}
+public class Calculator {
+    public static void main(String[] args) {
+
+        public List<Double> calList;
+        public Double calculator(int num1, int num2, char operator) {
+            // throw 어떻게 하는거지
+            double result = 0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("result = " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("result = " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("result = " + result);
+                break;
+            case '/':
+                if (num2 == 0) {throw ("");
+                }
+                result = (double) num1 / num2;
+                System.out.println("result = " + result);
+                break;
+            default:
+                // 무슨 코드를 넣어야 하는지
+
+        } return result;
+    }
+}}
